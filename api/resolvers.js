c689a24a7e864cfd742f7a5dc044208b134aaeb6
aroerick <a.roerick@gmail.com>
2018-07-12 09:24:53 -0700

    Initial commit.

---
+++ api/resolvers.js
@@ -0,0 +1,54 @@
+const data = require('./data')
+const resolvers = {
+    Query: {
+        movies() {
+            return data.movies
+        },
+        people() {
+            return data.people
+        },
+        person(root, { id }) {
+            return data.people.find(person => person.id === parseInt(id))
+        },
+        movie(root, { id }) {
+            return data.movies.find(movie => movie.id === parseInt(id))
+        }
+    },
+    Mutation: {
+        addPerson(root, variables) {
+            const newPerson = {
+                id: data.people.length + 1,
+                ...variables
+            }
+            data.people.push(newPerson)
+            return newPerson
+        }
+    },
+    Movie: {
+        director(movie) {
+            if (!movie.director) return null;
+            return data.people.find(person => person.id === movie.director)
+        },
+        stars(movie) {
+            return data.people.filter(person => (
+                person.filmography.find(credit => (
+                    credit === movie.id && person.id !== movie.director
+                ))
+            ))
+        }
+    },
+    Person: {
+        filmography(person) {
+            return data.movies.filter(movie => {
+                if (person.id === movie.director) {
+                    return movie
+                } else {
+                    return movie.stars.find(star => (
+                        star === person.id 
+                    ))
+                }
+            })
+        }
+    }
+}
+module.exports = resolvers
---
+++ api/schema.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express')
+
+module.exports = gql`
+
+    type Person {
+        id: ID!
+        name: String!
+        birthday: String
+        placeOfBirth: String
+        bio: String
+        filmography: [Movie]
+    }
+    type Movie {
+        id: ID!
+        title: String!
+        releaseDate: String
+        stars: [Person]
+        director: Person
+    }
+    type Mutation {
+        addPerson(
+            name: String!
+            birthday: String
+            placeOfBirth: String
+            bio: String
+            filmography: [ID]
+        ): Person
+    }
+    type Query {
+        people: [Person]
+        person(id: ID!): Person
+        movies: [Movie]
+        movie(id: ID!): Movie
+    }
+
+`
+
