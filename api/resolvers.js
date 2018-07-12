623a2400273590f50386efc0cb133f98db0b1cee
johnstonstu <stu.w.johnston@gmail.com>
2018-07-12 09:50:44 -0700

    add person input and mutation

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,54 +1,52 @@
-const data = require('./data')
+const data = require("./data");
 const resolvers = {
-    Query: {
-        movies() {
-            return data.movies
-        },
-        people() {
-            return data.people
-        },
-        person(root, { id }) {
-            return data.people.find(person => person.id === parseInt(id))
-        },
-        movie(root, { id }) {
-            return data.movies.find(movie => movie.id === parseInt(id))
-        }
+  Query: {
+    movies() {
+      return data.movies;
     },
-    Mutation: {
-        addPerson(root, variables) {
-            const newPerson = {
-                id: data.people.length + 1,
-                ...variables
-            }
-            data.people.push(newPerson)
-            return newPerson
-        }
+    people() {
+      return data.people;
     },
-    Movie: {
-        director(movie) {
-            if (!movie.director) return null;
-            return data.people.find(person => person.id === movie.director)
-        },
-        stars(movie) {
-            return data.people.filter(person => (
-                person.filmography.find(credit => (
-                    credit === movie.id && person.id !== movie.director
-                ))
-            ))
-        }
+    person(root, { id }) {
+      return data.people.find(person => person.id === parseInt(id));
     },
-    Person: {
-        filmography(person) {
-            return data.movies.filter(movie => {
-                if (person.id === movie.director) {
-                    return movie
-                } else {
-                    return movie.stars.find(star => (
-                        star === person.id 
-                    ))
-                }
-            })
-        }
+    movie(root, { id }) {
+      return data.movies.find(movie => movie.id === parseInt(id));
     }
-}
-module.exports = resolvers
+  },
+  Mutation: {
+    addPerson(root, variables, context, info) {
+      const newPerson = {
+        id: data.people.length + 1,
+        ...variables.person
+      };
+      data.people.push(newPerson);
+      return newPerson;
+    }
+  },
+  Movie: {
+    director(movie) {
+      if (!movie.director) return null;
+      return data.people.find(person => person.id === movie.director);
+    },
+    stars(movie) {
+      return data.people.filter(person =>
+        person.filmography.find(
+          credit => credit === movie.id && person.id !== movie.director
+        )
+      );
+    }
+  },
+  Person: {
+    filmography(person) {
+      return data.movies.filter(movie => {
+        if (person.id === movie.director) {
+          return movie;
+        } else {
+          return movie.stars.find(star => star === person.id);
+        }
+      });
+    }
+  }
+};
+module.exports = resolvers;
--- api/schema.js
+++ api/schema.js
@@ -1,37 +1,44 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require("apollo-server-express");
 
 module.exports = gql`
-
-    type Person {
-        id: ID!
-        name: String!
-        birthday: String
-        placeOfBirth: String
-        bio: String
-        filmography: [Movie]
-    }
-    type Movie {
-        id: ID!
-        title: String!
-        releaseDate: String
-        stars: [Person]
-        director: Person
-    }
-    type Mutation {
-        addPerson(
-            name: String!
-            birthday: String
-            placeOfBirth: String
-            bio: String
-            filmography: [ID]
-        ): Person
-    }
-    type Query {
-        people: [Person]
-        person(id: ID!): Person
-        movies: [Movie]
-        movie(id: ID!): Movie
-    }
-
-`
-
+  type Person {
+    id: ID!
+    name: String!
+    birthday: String
+    placeOfBirth: String
+    bio: String
+    filmography: [Movie]
+  }
+  type Movie {
+    id: ID!
+    title: String!
+    releaseDate: String
+    stars: [Person]
+    director: Person
+  }
+  #   type Mutation {
+  #     addPerson(
+  #       name: String!
+  #       birthday: String
+  #       placeOfBirth: String
+  #       bio: String
+  #       filmography: [ID]
+  #     ): Person
+  #   }
+  type Query {
+    people: [Person]
+    person(id: ID!): Person
+    movies: [Movie]
+    movie(id: ID!): Movie
+  }
+  input PersonInput {
+    name: String!
+    birthday: String
+    placeOfBirth: String
+    bio: String
+    filmography: [ID]
+  }
+  type Mutation {
+    addPerson(person: PersonInput!): Person
+  }
+`;
